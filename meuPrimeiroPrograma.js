f8d1da62964950961b5e8a9f1e2843c0f289e0ab
4rthurWillian <pohlod.arthur@escola.pr.gov.br>
2025-08-29 07:44:01 -0300

    Conversão de tipos

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro programa: trabalho com variáveis")
+
+const idade = 15;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade+2);
+console.log(idade/2);
+
+ 
---
+++ trabahandocomVariaveis.js
@@ -0,0 +1,11 @@
+//console.log("Trabalhado com variáveis");
+console.log("Trabalhado com variáveis");
+//JS é case sensitive = 29;
+
+const idade = 29;
+
+const Idade = 15;
+
+const nome = "Ricardo";
+
+console.log(nome);
