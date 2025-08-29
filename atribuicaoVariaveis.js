32a610bad8df07c7bb73e55d0ca705df3a9e7767
4rthurWillian <pohlod.arthur@escola.pr.gov.br>
2025-08-29 09:19:26 -0300

    .

---
+++ atribuicaoVariaveis.js
@@ -0,0 +1,18 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const primeiroNome = "Arthur";
+const sobrenome = "Willian Ota Pohlod";
+
+console.log('Meu nome é ${primeiroNome} ${sobrenome}');
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console. log(nomeCompleto);
+
+let IdleDeadline; // declarando variáveis
+idade = 16; // atribuindo valor
+
+idade = idade + 1;
+crossOriginIsolated.log(idade);
